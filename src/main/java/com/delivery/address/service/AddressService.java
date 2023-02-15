d1168354873fa7d08fc084d55f958f84a7f9dead
Abner Werley <abnerwerley77@gmail.com>
2023-02-15 16:52:21 -0300

    :sparkles: feat(Adress): fetching data from api

---
+++ src/main/java/com/delivery/address/service/AddressService.java
@@ -0,0 +1,32 @@
+package com.delivery.address.service;
+
+import com.delivery.address.dto.AddressTO;
+import com.delivery.exception.RequestException;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+@Setter
+public class AddressService {
+
+    @Autowired
+    private WebClient webClient;
+
+    public AddressTO getAddress(String cep) {
+        try {
+            Mono<AddressTO> monoAddress = webClient.method(HttpMethod.GET)
+                    .uri("/{cep}/json", cep)
+                    .retrieve()
+                    .bodyToMono(AddressTO.class);
+
+            return monoAddress.block();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RequestException("Could not find Address by cep : " + cep);
+        }
+    }
+}
---
+++ src/main/java/com/delivery/exception/RequestException.java
@@ -0,0 +1,15 @@
+package com.delivery.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Getter
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class RequestException extends RuntimeException {
+    private final String message;
+
+    public RequestException(String message) {
+        this.message = message;
+    }
+}
