546330b29057502d87caa563f00cf35050257156
Abner Werley <abnerwerley77@gmail.com>
2023-02-15 21:27:46 -0300

    :goal_net: errors: added pattern to exception messages

---
+++ src/main/java/com/delivery/exception/ExceptionResponseDetails.java
@@ -0,0 +1,65 @@
+package com.delivery.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ExceptionResponseDetails {
+    private String title;
+    private int status;
+    private String detail;
+    private long timestamp;
+    private String message;
+
+    private ExceptionResponseDetails() {
+    }
+
+    public static final class Builder {
+        private String title;
+        private int status;
+        private String detail;
+        private long timestamp;
+        private String message;
+
+        private Builder() {
+        }
+
+        public static Builder newBuilder() {
+            return new Builder();
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder status(int status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder detail(String detail) {
+            this.detail = detail;
+            return this;
+        }
+
+        public Builder timestamp(long timestamp) {
+            this.timestamp = timestamp;
+            return this;
+        }
+
+        public Builder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public ExceptionResponseDetails build() {
+            ExceptionResponseDetails exceptionResponseDetails = new ExceptionResponseDetails();
+            exceptionResponseDetails.timestamp = this.timestamp;
+            exceptionResponseDetails.detail = this.detail;
+            exceptionResponseDetails.title = this.title;
+            exceptionResponseDetails.message = this.message;
+            exceptionResponseDetails.status = this.status;
+            return exceptionResponseDetails;
+        }
+    }
+}
