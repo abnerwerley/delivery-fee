fb9ad2362410f8b97a44dcf38c47c66e9ad7f542
Abner Werley <abnerwerley77@gmail.com>
2023-02-15 22:22:19 -0300

    :fire: remove: removing remaining code related to webflux

--- src/main/java/com/delivery/address/config/AddressClient.java
+++ src/main/java/com/delivery/address/config/AddressClient.java
@@ -2,21 +2,10 @@ package com.delivery.address.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AddressClient {
-
-    @Bean
-    public WebClient webClientAddress(WebClient.Builder builder) {
-        return builder
-                .baseUrl("viacep.com.br/ws")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/vnd.api+json")
-                .defaultHeader(HttpHeaders.ACCEPT, "application/vnd.api+json")
-                .build();
-    }
 
     @Bean
     public RestTemplate restTemplate() {
