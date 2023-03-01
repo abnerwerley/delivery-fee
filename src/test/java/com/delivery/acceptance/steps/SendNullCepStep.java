ced1985d52946b5caa01813d408876783861a36a
Abner Werley <abnerwerley77@gmail.com>
2023-03-01 17:44:45 -0300

    :recycle: refactor(cucumber): refactoring cucumber tests

--- src/test/java/com/delivery/acceptance/steps/SendNullCepStep.java
+++ src/test/java/com/delivery/acceptance/steps/SendNullCepStep.java
@@ -1,6 +1,5 @@
 package com.delivery.acceptance.steps;
 
-
 import com.delivery.address.service.AddressService;
 import com.delivery.exception.RequestException;
 import com.delivery.fee.controller.FeeController;
@@ -9,38 +8,32 @@ import com.delivery.fee.service.FeeService;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.jupiter.api.Assertions;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SendNullCepStep {
 
+    private final CepForm form = new CepForm(CEP);
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final AddressService addressService = new AddressService(restTemplate);
+    private final FeeService service = new FeeService(addressService);
+    private final FeeController controller = new FeeController(service);
+
     public static final String CEP = null;
 
     @Given("null cep")
     public void null_cep() {
-        CepForm form = new CepForm(CEP);
+        new CepForm(CEP);
     }
 
     @When("request is made with null cep")
     public void request_is_made_with_null_cep() {
-        CepForm form = new CepForm(CEP);
-        RestTemplate restTemplate = new RestTemplate();
-        AddressService addressService = new AddressService(restTemplate);
-        FeeService service = new FeeService(addressService);
-        FeeController controller = new FeeController(service);
-        Exception exception = Assertions.assertThrows(RequestException.class, () -> controller.getDeliveryFeeByCep(form));
-        assertNotNull(exception);
+        assertThrows(RequestException.class, () -> controller.getDeliveryFeeByCep(form));
     }
 
     @Then("RequestException is thrown explaining that field cep is mandatory")
     public void requestException_is_thrown_explaining_that_field_cep_is_mandatory() {
-        CepForm form = new CepForm(CEP);
-        RestTemplate restTemplate = new RestTemplate();
-        AddressService addressService = new AddressService(restTemplate);
-        FeeService service = new FeeService(addressService);
-        FeeController controller = new FeeController(service);
         Exception exception = assertThrows(RequestException.class, () -> controller.getDeliveryFeeByCep(form));
         assertNotNull(exception);
         assertEquals("Cep is mandatory.", exception.getMessage());
--- src/test/java/com/delivery/acceptance/steps/SendingValidCepStep.java
+++ src/test/java/com/delivery/acceptance/steps/SendingValidCepStep.java
@@ -9,41 +9,37 @@ import com.delivery.fee.service.FeeService;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.jupiter.api.Assertions;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SendingValidCepStep {
 
+    private final CepForm form = new CepForm(CEP);
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final AddressService addressService = new AddressService(restTemplate);
+    private final FeeService service = new FeeService(addressService);
+    private final FeeController controller = new FeeController(service);
     public static final String CEP = "04851280";
 
     @Given("valid cep")
     public void valid_cep() {
-        CepForm form = new CepForm(CEP);
+        new CepForm(CEP);
     }
 
     @When("request is made")
     public void request_is_made() {
-        CepForm form = new CepForm(CEP);
-        RestTemplate restTemplate = new RestTemplate();
-        AddressService addressService = new AddressService(restTemplate);
-        FeeService service = new FeeService(addressService);
-        FeeController controller = new FeeController(service);
-        Assertions.assertNotNull(controller.getDeliveryFeeByCep(form));
+        controller.getDeliveryFeeByCep(form);
     }
 
     @Then("FeeResponse is returned according to cep")
     public void feeResponse_is_returned_according_to_cep() {
-        CepForm form = new CepForm(CEP);
-        RestTemplate restTemplate = new RestTemplate();
-        AddressService addressService = new AddressService(restTemplate);
-        FeeService service = new FeeService(addressService);
-        FeeController controller = new FeeController(service);
         FeeResponse response = controller.getDeliveryFeeByCep(form);
-        Assertions.assertNotNull(response);
+        assertNotNull(response);
         assertEquals(new BigDecimal("7.85"), response.getFrete());
+        assertEquals("SP", response.getEstado());
     }
 }
